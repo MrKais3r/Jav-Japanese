8df3ea95564969baa6b5431eb5dc340ce9647f53
24-akshitsinghal <akshitsinghal2424@gmail.com>
2025-12-06 00:34:16 +0530

    added navigation

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -17,6 +17,33 @@ const backendApiCall = async () => {
   return text;
 };
 
+const lessons = [
+  { id: 0, title: "Writing System, Greetings, and Numbers" },
+  { id: 1, title: "New Friends" },
+  { id: 2, title: "Shopping" },
+  { id: 3, title: "Making a Date" },
+  { id: 4, title: "The First Date" },
+  { id: 5, title: "A Trip to Okinawa" },
+  { id: 6, title: "A Day in Robert's Life" },
+  { id: 7, title: "Family Picture" },
+  { id: 8, title: "Barbecue" },
+  { id: 9, title: "Kabuki" },
+  { id: 10, title: "Winter Vacation Plans" },
+  { id: 11, title: "After the Vacation" },
+  { id: 12, title: "Feeling Ill" },
+  { id: 13, title: "Looking for a Part-time Job" },
+  { id: 14, title: "Valentine's Day" },
+  { id: 15, title: "A Trip to Nagano" },
+  { id: 16, title: "Lost and Found" },
+  { id: 17, title: "Grumble and Gossip" },
+  { id: 18, title: "John's Part-time Job" },
+  { id: 19, title: "Meeting the Boss" },
+  { id: 20, title: "Mary Goes Shopping" },
+  { id: 21, title: "Burglar" },
+  { id: 22, title: "Education in Japan" },
+  { id: 23, title: "Good-bye" },
+];
+
 export default async function Home() {
   const message = await backendApiCall();
 
@@ -58,6 +85,31 @@ export default async function Home() {
           >
             <Link href="/quick-navigation">Quick Navigation →</Link>
           </Button>
+        </CardContent>
+      </Card>
+
+      <Card className="border max-w-6xl w-full border-white/10 bg-background/60 backdrop-blur-md shadow-xl">
+        <CardHeader>
+          <CardTitle className="text-muted-foreground">
+            Quick Navigation
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent className="flex flex-col gap-6 p-6">
+          {/* Lessons */}
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2">
+              {lessons.map((lesson) => (
+                <Link
+                  key={lesson.id}
+                  href={`/lesson/${lesson.id}`}
+                  className="text-sm px-3 py-2 rounded-md hover:bg-gray-800 hover:text-white transition border border-white/5"
+                >
+                  Lesson {lesson.id}: {lesson.title}
+                </Link>
+              ))}
+            </div>
+          </div>
         </CardContent>
       </Card>
     </div>
